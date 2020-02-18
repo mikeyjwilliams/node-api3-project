02f57608c8966e81bdbb50c873159e808a6f4e1a
Michael Williams <manicdesigner@gmail.com>
2020-02-18 00:15:07 -0500

    404 page, 500 error and logger added

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
 const server = express();
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use((req, res) => {
+  res.status(404).json({ message: '404 page not found fail whale' });
+});
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: 'Sorry something went wrong' });
+});
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, url, originalUrl, path } = req;
+  const time = new Date();
+  console.log(`${method} ${url} ${originalUrl + '/' + path} ${time}`);
+  next();
+}
 
 module.exports = server;
