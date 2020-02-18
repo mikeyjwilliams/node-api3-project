fbf2cbe19e1e74f8cc2567eab14b1335e02b8cb3
Michael Williams <manicdesigner@gmail.com>
2020-02-18 00:13:38 -0500

    built index

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const port = 5000;
+const host = 'localhost';
+const server = require('./server');
+
+server.listen(port, host, () => {
+  console.log(`\n *** ${host}:${port} *** \n`);
+});
