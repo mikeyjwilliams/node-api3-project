10a1d072e8ab6b125c63c76fd2ee2acc3b6c5b5f
Michael Williams <manicdesigner@gmail.com>
2020-02-18 14:04:48 -0500

    add logger back into server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = express();
 server.use(express.json());
 
 const userRouter = require('./users/userRouter');
-// server.use(logger);
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
