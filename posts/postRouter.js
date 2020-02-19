9def19bc96a5fb44ccecd2e6a0f595209471a8f4
Mikey Williams <manicdesigner@gmail.com>
2020-02-18 23:10:29 -0500

    post get

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,53 @@
 const express = require('express');
+const postDb = require('./postDb');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', async (req, res, next) => {
+	try {
+		const posts = postDb.get();
+		console.log(posts);
+		res.status(200).json(posts);
+	} catch (err) {
+		console.log(err);
+		next(err);
+	}
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
+// router.get('/:id', validatePostId(), async (req, res, next) => {
+// 	// do your magic!
+// });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
+// router.delete('/:id', (req, res) => {
+// 	// do your magic!
+// });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
+// router.put('/:id', (req, res) => {
+// 	// do your magic!
+// });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+function validatePostId(postId) {
+	return (req, res, next) => {
+		postDb
+			.getById(req.params.id)
+			.then((post) => {
+				if (post) {
+					req.post = post;
+				} else {
+					return res
+						.status(400)
+						.json({ message: 'specific post ID not found' });
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				next(err);
+			});
+
+		next();
+	};
 }
 
 module.exports = router;
