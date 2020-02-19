1059e72176bd39db681582dbdf8767931e2049f5
Mikey Williams <manicdesigner@gmail.com>
2020-02-18 19:12:09 -0500

    format server file

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ const userRouter = require('./users/userRouter');
 server.use(logger);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 server.use('/api/users', userRouter);
 
 server.use((req, res) => {
-  res.status(404).json({ message: '404 page not found fail whale' });
+	res.status(404).json({ message: '404 page not found fail whale' });
 });
 
 server.use((err, req, res, next) => {
-  res.status(500).json({ message: 'Sorry something went wrong' });
+	res.status(500).json({ message: 'Sorry something went wrong' });
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-  const { method, url, originalUrl, path } = req;
-  const time = new Date();
-  console.log(`${method} ${url} ${originalUrl} ${time}`);
-  next();
+	const { method, url, originalUrl, path } = req;
+	const time = new Date();
+	console.log(`${method} ${url} ${originalUrl} ${time}`);
+	next();
 }
 
 module.exports = server;
